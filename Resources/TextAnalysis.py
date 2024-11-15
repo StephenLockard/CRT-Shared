f19056d96468a9e7655d8b19843701ad9992b728
Stephen Lockard <slockard@copado.com>
2024-11-15 12:25:20 -0500

    fix

--- Resources/TextAnalysis.py
+++ Resources/TextAnalysis.py
@@ -1,21 +1,13 @@
 import re
 import nltk
 from nltk.sentiment import SentimentIntensityAnalyzer
+from robot.api.deco import keyword
 
+class SentimentAnalysis:
     def __init__(self):
         nltk.download('vader_lexicon', quiet=True)
         self.sia = SentimentIntensityAnalyzer()
-        self.affirmative_patterns = [
-            r'\byes\b', r'\byeah\b', r'\byep\b', r'\bsure\b', r'\bcorrect\b', 
-            r'\baffirmative\b', r'\bindeed\b', r'\bagreed\b', r'\bconfirm\b'
-        ]
-        self.negative_patterns = [
-            r'\bno\b', r'\bnope\b', r'\bnah\b', r'\bincorrect\b', r'\bnegative\b', 
-            r'\bdisagree\b', r'\bdeny\b', r'\brefuse\b', r'\breject\b'
-        ]
 
     def analyze_sentiment(self, text):
         sentiment_score = self.sia.polarity_scores(text)['compound']
         return sentiment_score
-
-   
